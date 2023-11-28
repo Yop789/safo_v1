4b42937a58b0248fe7cfe907b401a0535016c701
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-11-27 18:47:33 -0600

    funcionalidad de agregar receta

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,17 @@
+import { Ingredient } from "./ingredient";
+import { Step } from "./step";
+import { Qualification } from "./qualification";
+
+export interface Recipe {
+    idUser: string;
+    name: string;
+    description: string;
+    qualification: Qualification[];
+    duration: number;
+    type: string;
+    difficulty: string;
+    category: string;
+    image: string;
+    ingredients: Ingredient[];
+    steps: Step[];
+  }
